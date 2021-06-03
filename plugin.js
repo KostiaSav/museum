1ffe54b3d0f0f8b1a4e9e1b77b6c131b5d323408
KostiaSav <75932621+KostiaSav@users.noreply.github.com>
2021-06-03 13:02:10 +0300

    Add files via upload

--- plugin.js
+++ plugin.js
@@ -19,3 +19,27 @@ function scenesMouseOut(numbers){
     loupe[numbers].style.opacity = 0;
 
 }
+
+const menuLinks = document.querySelectorAll('.container li a[data-goto]')
+
+if(menuLinks.length > 0){
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
+    });
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+            console.log(gotoBlockValue);
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            })
+
+            e.preventDefault();
+        }
+    }
+}
